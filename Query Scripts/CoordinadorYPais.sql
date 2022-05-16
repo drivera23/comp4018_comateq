5bcdbb2487c44d85f82962b06d3f32bdf89b2d21
amprosk <54246562+amprosk@users.noreply.github.com>
2022-05-16 18:07:35 -0400

    Update CoordinadorYPais.sql
    Included AS statements

--- Query Scripts/CoordinadorYPais.sql
+++ Query Scripts/CoordinadorYPais.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(Coordinador.NombreP, ' ', Coordinador.ApellidoP, ' ', Coordinador.ApellidoM), Pais.Nombre 
+SELECT CONCAT(Coordinador.NombreP, ' ', Coordinador.ApellidoP, ' ', Coordinador.ApellidoM) AS FullName, Pais.Nombre AS Pais
 FROM Coordinador, Pais, Universidad 
 WHERE Universidad.idUniversidad = Coordinador.idUniversidad 
 AND Universidad.idPais = Pais.idPais;  
